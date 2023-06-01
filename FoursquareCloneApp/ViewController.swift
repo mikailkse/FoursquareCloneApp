8ec17e7c145d791b8faeb7971107c0ebea874b85
Mikail Köse <41960652+mikailkse@users.noreply.github.com>
2023-06-01 23:49:44 +0300

    first commit

---
+++ FoursquareCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareCloneApp
+//
+//  Created by Mikail Köse on 1.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
